4ded506a34aa395825a9482c737be584fc9c90d3
bashbunni <bashbunni.io@gmail.com>
2021-11-15 12:37:55 -0800

    feat: add borders and active colors to Nav

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
       <nav className="nav">
         <div className="container">
           <ul className="nav-list">
-            <li className="nav-list__item">yoga</li>
+            <li className="nav-list__item nav-list__item--active">yoga</li>
             <li className="nav-list__item">water</li>
             <li className="nav-list__item">meditation</li>
           </ul>
