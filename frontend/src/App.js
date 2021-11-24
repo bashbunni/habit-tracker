ab61d890b3ff1eff6f969da40e9d3e19dc7ed133
bashbunni <bashbunni.io@gmail.com>
2021-11-24 10:53:16 -0800

    feat: add hamburger to open nav modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
-import "./App.scss";
 import Modal from "./components/Modal";
+import Habit from "./components/Habit";
+import hamburger from "./assets/icons/hamburger.svg";
+import "./App.scss";
 
 const App = () => {
-  const [navModal, setNavModal] = useState(true);
+  const [navModal, setNavModal] = useState(false);
+  const openNav = () => setNavModal(true);
   const closeNav = () => setNavModal(false);
 
   return (
-    <div className="App">
-      {navModal ? <Modal closeModal={closeNav} /> : null}
-      <h1>Habit Tracker</h1>
+    <div className="app">
+      <div className="container">
+        <img
+          className="hamburger"
+          src={hamburger}
+          alt="open menu"
+          onClick={openNav}
+        />
+        {navModal ? <Modal closeModal={closeNav} /> : null}
+        <Habit />
+        <h1>Habit Tracker</h1>
+      </div>
     </div>
   );
 };
