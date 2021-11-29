1b8f4ed5e3ed655b11d18383544a1e353545abf8
bashbunni <bashbunni.io@gmail.com>
2021-11-29 15:10:22 -0800

    fix: fix wails backend

--- main.go
+++ main.go
@@ -9,20 +9,12 @@ func basic() string {
   return "World!"
 }
 
-//go:embed frontend/build/static/js/main.js
-var js string
-
-//go:embed frontend/build/static/css/main.css
-var css string
-
 func main() {
 
   app := wails.CreateApp(&wails.AppConfig{
     Width:  1024,
     Height: 768,
     Title:  "habit_tracker",
-    JS:     js,
-    CSS:    css,
     Colour: "#131313",
   })
   app.Bind(basic)
