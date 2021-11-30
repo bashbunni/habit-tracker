0e50c4545dcd9493a2414ba97dca22cb0378fb65
bashbunni <bashbunni.io@gmail.com>
2021-11-30 08:12:06 -0800

    fix: fix trying to setState on an unmounted component error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,9 +17,7 @@ const App = () => {
   useEffect(() => {
     if (mountedRef.current) {
       window.backend.Habits.GetHabits().then((response) => {
-        console.log(response);
         setHabitList(response);
-        // TODO: why is this habitList still null? It says it's trying to set state on unmounted component
       });
     }
     return () => (mountedRef.current = false);
@@ -27,38 +25,40 @@ const App = () => {
 
   return (
     <div className="app">
-      <div className="container">
-        <img
-          className="hamburger"
-          src={hamburger}
-          alt="open menu"
-          onClick={openNav}
-        />
-        <Router>
-          <Switch>
-            {navModal ? (
-              <Modal closeModal={closeNav} habitList={habitList} />
-            ) : null}
-            <Route path="/pomodoro" exact component={Pomodoro} />
-            <Route
-              path="/new"
-              exact
-              render={(props) => (
-                <AddHabit setHabitList={setHabitList} habitList={habitList} />
-              )}
-            />
-            <Route
-              path="/:name"
-              render={(props) => <Habit habitList={habitList} />}
-            />
-            <Route
-              path="/"
-              exact
-              render={(props) => <Habit habitList={habitList} />}
-            />
-          </Switch>
-        </Router>
-      </div>
+      {habitList && (
+        <div className="container">
+          <img
+            className="hamburger"
+            src={hamburger}
+            alt="open menu"
+            onClick={openNav}
+          />
+          <Router>
+            <Switch>
+              {navModal ? (
+                <Modal closeModal={closeNav} habitList={habitList} />
+              ) : null}
+              <Route path="/pomodoro" exact component={Pomodoro} />
+              <Route
+                path="/new"
+                exact
+                render={(props) => (
+                  <AddHabit setHabitList={setHabitList} habitList={habitList} />
+                )}
+              />
+              <Route
+                path="/:name"
+                render={(props) => <Habit habitList={habitList} />}
+              />
+              <Route
+                path="/"
+                exact
+                render={(props) => <Habit habitList={habitList} />}
+              />
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 };
