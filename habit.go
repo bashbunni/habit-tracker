7b11f96cf08dc978176d9e846ab7e365fe41b2db
bashbunni <bashbunni.io@gmail.com>
2021-11-29 22:05:51 -0800

    refactor: organize and comment code to denote mock data

--- habit.go
+++ habit.go
@@ -9,10 +9,37 @@ type Habit struct {
 	Why      string `json:"why"`
 }
 
+// create a new Habit
 func NewHabit(Id uint, Name string, Unit string, Pomodoro bool, Why string) *Habit {
-	return &Habit{}
+	return &Habit{Id: Id, Name: Name, Unit: Unit, Pomodoro: Pomodoro, Why: Why}
 }
 
+/* ******
+ * mocks
+ * ******/
+// TODO: move to separate mock file when DB is implemented
+
+type Habits struct {
+	habits []*Habit
+}
+
+// mock database
+func (h *Habits) NewHabits() {
+	h.habits = []*Habit{
+		{Id: 1, Name: "yoga", Unit: "hours", Pomodoro: false, Why: "I want to do yoga so I can be more relaxed"},
+		{Id: 2, Name: "meditation", Unit: "15 minutes", Pomodoro: false, Why: "I want to meditate so I can be more present"},
+		{Id: 3, Name: "hydration", Unit: "litres", Pomodoro: false, Why: "I want to be hydrated so I can feel better"},
+	}
+}
+
+// mock add habit to list of habits
+// return true if habit is added successfully
+func (h Habits) AddHabit(habit *Habit) bool {
+	h.habits = append(h.habits, habit)
+	return true
+}
+
+// mock data for getting habits
 func GetHabits() []*Habit {
 	var habits = []*Habit{
 		{Id: 1, Name: "yoga", Unit: "hours", Pomodoro: false, Why: "I want to do yoga so I can be more relaxed"},
