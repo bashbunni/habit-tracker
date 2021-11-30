f28ad0b3fd2108452183a4ec4fa27b810f71c528
bashbunni <bashbunni.io@gmail.com>
2021-11-29 20:41:50 -0800

    feat: set habit list in state from habit list in backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Modal from "./components/Modal";
 import Habit from "./components/Habit";
@@ -8,29 +8,17 @@ import hamburger from "./assets/icons/hamburger.svg";
 import "./App.scss";
 
 const App = () => {
-  const [habitList, setHabitList] = useState([
-    {
-      id: 1,
-      name: "yoga",
-      unit: "hours",
-      why: "I want to do yoga so I can be more relaxed",
-    },
-    {
-      id: 2,
-      name: "meditation",
-      unit: "15 minutes",
-      why: "I want to meditate so I can be more present",
-    },
-    {
-      id: 3,
-      name: "hydration",
-      unit: "litres",
-      why: "I want to be hydrated so I can feel better",
-    },
-  ]);
+  const [habitList, setHabitList] = useState([]);
   const [navModal, setNavModal] = useState(false);
   const openNav = () => setNavModal(true);
   const closeNav = () => setNavModal(false);
+
+  useEffect(() => {
+    window.backend.GetHabits().then((response) => {
+      console.log(response);
+      setHabitList(response);
+    });
+  }, []);
 
   return (
     <div className="app">
