3bf9f63946996d919335b3a0957d9ac199dcde8a
bashbunni <bashbunni.io@gmail.com>
2021-11-29 20:32:54 -0800

    feat: access Habits from frontend

--- habit.go
+++ habit.go
@@ -1,22 +1,23 @@
 package main
 
 type Habit struct {
-	id       uint
-	name     string
-	unit     string
-	pomodoro bool
-	why      string
+	// need to start with an uppercase letter to export it
+	Id       uint   `json:"id"`
+	Name     string `json:"name"`
+	Unit     string `json:"unit"`
+	Pomodoro bool   `json:"pomodoro"`
+	Why      string `json:"why"`
 }
 
-func NewHabit(id uint, name string, unit string, pomodoro bool, why string) *Habit {
+func NewHabit(Id uint, Name string, Unit string, Pomodoro bool, Why string) *Habit {
 	return &Habit{}
 }
 
-func GetHabits() []Habit {
-	var habits = []Habit{
-		Habit{id: 1, name: "yoga", unit: "hours", pomodoro: false, why: "I want to do yoga so I can be more relaxed"},
-		Habit{id: 2, name: "meditation", unit: "15 minutes", pomodoro: false, why: "I want to meditate so I can be more present"},
-		Habit{id: 3, name: "hydration", unit: "litres", pomodoro: false, why: "I want to be hydrated so I can feel better"},
+func GetHabits() []*Habit {
+	var habits = []*Habit{
+		{Id: 1, Name: "yoga", Unit: "hours", Pomodoro: false, Why: "I want to do yoga so I can be more relaxed"},
+		{Id: 2, Name: "meditation", Unit: "15 minutes", Pomodoro: false, Why: "I want to meditate so I can be more present"},
+		{Id: 3, Name: "hydration", Unit: "litres", Pomodoro: false, Why: "I want to be hydrated so I can feel better"},
 	}
 	return habits
 }
