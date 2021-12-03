71172d996c5cd800bfb3ee177dad4fe20313b6d7
bashbunni <bashbunni.io@gmail.com>
2021-12-03 11:58:58 -0800

    feat: update habit in DB

--- HabitRepository.go
+++ HabitRepository.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"log"
 )
 
 type HabitRepository interface {
 	GetHabits() []Habit
 	AddHabit(Habit) error
+	EditHabit(Habit) Habit
+	DeleteHabit(uint) bool
 }
 
 func GetHabits() []Habit {
@@ -17,6 +19,7 @@ func GetHabits() []Habit {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer results.Close()
 	var habits []Habit
 	for results.Next() {
 		var habit Habit
@@ -27,6 +30,18 @@ func GetHabits() []Habit {
 		}
 	}
 	return habits
+}
+
+func EditHabit(habit Habit) Habit {
+	db := DBConnect()
+	defer db.Close()
+	_, err := db.Exec("UPDATE habit SET habit_id = ?, habit_name = ?, habit_unit = ?, habit_pomodoro = ?, habit_why = ? WHERE habit_id = ?", habit.ID, habit.Name, habit.Unit, habit.Pomodoro, habit.Why, habit.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := db.Exec("SELECT * FROM habit WHERE habit_id = ?", habit.ID)
+	fmt.Println(res)
+	return Habit{}
 }
 
 func AddHabit(habit Habit) error {
