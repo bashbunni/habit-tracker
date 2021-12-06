bce56c0641f46e53f0a8698c95442ad9d4a7ec95
bashbunni <bashbunni.io@gmail.com>
2021-12-06 12:38:37 -0800

    feat: add Date from frontend

--- date.go
+++ date.go
@@ -1,9 +1,25 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 type Date struct {
 	ID      string `json:"date"`
 	Count   uint   `json:"count"`
 	HabitID uint   `json:"habit_id"`
+}
+
+func NewDateFromJSON(req []byte) Date {
+	fmt.Println(req)
+	var date Date
+	err := json.Unmarshal(req, &date)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return date
 }
 
 func NewDate(ID string, Count uint, HabitID uint) Date {
--- date_repository.go
+++ date_repository.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+	"time"
 )
 
 type DateRepository interface {
 	GetAllDates(uint) []Date
+	GetTodaysDate() Date
 	AddDate(Date) error
+	AddCount(Date) error
 }
 
 // GetAllDates gets all dates for a given habit.
@@ -29,13 +33,48 @@ func (s MySQLRepository) GetAllDates(habit_id uint) []Date {
 	return dates
 }
 
+// GetAllDates gets all dates for a given habit.
+func (s MySQLRepository) GetTodaysDate() Date {
+	today := time.Now().UTC().Format("2006-01-02")
+	var date Date
+	err := s.DB.QueryRow("SELECT * FROM date WHERE date_id = ?", today).Scan(&date.ID, &date.Count, &date.HabitID)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			// real error
+			log.Fatal(err)
+		}
+	}
+	return date
+}
+
+func (s MySQLRepository) TodayExists() bool {
+	today := s.GetTodaysDate()
+	if today.ID == "" {
+		return false
+	}
+	return true
+
+}
+
 func (s MySQLRepository) AddDate(date Date) error {
-	insert := fmt.Sprintf("INSERT INTO date(date_id, date_count, habit_id) VALUES (%s, %d, %d)", date.ID, date.Count, date.HabitID)
+	fmt.Println(date.ID)
+	insert := fmt.Sprintf("INSERT INTO date(date_id, date_count, habit_id) VALUES ('%s', %d, %d)", date.ID, date.Count, date.HabitID)
+	fmt.Println(insert)
 	_, err := s.DB.Exec(insert)
 	return err
 }
 
 func (s MySQLRepository) AddCount(date Date) error {
-	_, err := s.DB.Exec("UPDATE date SET date_count = ? WHERE date_id = ? AND habit_id = ?", date.Count, date.ID, date.HabitID)
-	return err
+	if s.TodayExists() {
+		// TODO: make this increment the count instead of overwrite
+		updateCount := fmt.Sprintf("UPDATE date SET date_count = %d WHERE date_id = '%s' AND habit_id = %d", date.Count, date.ID, date.HabitID)
+		_, err := s.DB.Exec(updateCount)
+		return err
+	} else {
+		return s.AddDate(date)
+	}
+}
+
+func (s MySQLRepository) AddCountFromJSON(req []byte) error {
+	return s.AddCount(JSONToDate(req))
 }
